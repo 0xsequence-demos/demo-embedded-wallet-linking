9c8c91d7893fb66261660889f1d489d00aa586db
moskalyk <morgan.moskalyk@protonmail.ch>
2024-08-02 15:44:11 -0400

    update to new sequence kit

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,4 @@
-import { KitConfig, getKitConnectWallets } from '@0xsequence/kit'
-import { getDefaultConnectors, getDefaultWaasConnectors, mock } from '@0xsequence/kit-connectors'
+import { KitConfig, getKitConnectWallets, getDefaultConnectors } from '@0xsequence/kit'
 import { findNetworkConfig, allNetworks } from '@0xsequence/network'
 import { Transport, zeroAddress } from 'viem'
 import { createConfig, http } from 'wagmi'
@@ -7,16 +6,10 @@ import { Chain, arbitrumNova, arbitrumSepolia, mainnet, polygon } from 'wagmi/ch
 
 const projectAccessKey = 'pk_live_4e4b3b4b-1b4b-4b4b-8b4b-4b4b4b4b4b4b'
 
-const chains = [arbitrumNova, arbitrumSepolia, mainnet, polygon] as const satisfies Chain[]
-const transports = chains.reduce<Record<number, Transport>>((acc, chain) => {
-  const network = findNetworkConfig(allNetworks, chain.id)
-
-  if (network) {
-    acc[chain.id] = http(network.rpcUrl)
-  }
-
-  return acc
-}, {})
+import { getDefaultChains } from '@0xsequence/kit'
+ 
+const chains = [arbitrumNova, arbitrumSepolia, mainnet, polygon] as const satisfies Chain[] // optionally, supply an array of chain ID's getDefaultChains([1,137])
+const transports = Object.fromEntries(chains.map(chain => [chain.id, http()]));
 
 chains.forEach(chain => {
   const network = findNetworkConfig(allNetworks, chain.id)
@@ -37,6 +30,7 @@ const getUniversalConnectors = () => {
 }
 
 export const wagmiConfig = createConfig({
+  // @ts-ignore
   transports,
   chains,
   connectors: getUniversalConnectors()
