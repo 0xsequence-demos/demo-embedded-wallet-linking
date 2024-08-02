e0b3deb182930af8d904ff4a8eb2c4382144f433
moskalyk <morgan.moskalyk@protonmail.ch>
2024-08-02 15:48:12 -0400

    update vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,8 +6,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/demo-waas-wallet-link/",
-  server: {
-    port: 3006,
-  },
+  base: ""
 });
