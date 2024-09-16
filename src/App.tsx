1a20fddccdf6ec53f79cb51db40a4f4ca26a1d22
Tolgahan Arikan <tolgahan.arikan@gmail.com>
2024-09-16 12:05:42 +0300

    Changes for updated api + update packages, remove checkout lib

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { WagmiProvider } from "wagmi";
 import { Homepage } from "./pages/Homepage";
 import { kitConfig, wagmiConfig } from "./config";
 import { KitWalletProvider } from "@0xsequence/kit-wallet";
-import { KitCheckoutProvider } from "@0xsequence/kit-checkout";
 
 const queryClient = new QueryClient();
 
@@ -18,15 +17,13 @@ export const App = () => {
       <QueryClientProvider client={queryClient}>
         <KitProvider config={kitConfig}>
           <KitWalletProvider>
-            <KitCheckoutProvider>
-              <div id="app">
-                <ThemeProvider root="#app" scope="app" theme="dark">
-                  <ToastProvider duration={2500}>
-                    <Homepage />
-                  </ToastProvider>
-                </ThemeProvider>
-              </div>
-            </KitCheckoutProvider>
+            <div id="app">
+              <ThemeProvider root="#app" scope="app" theme="dark">
+                <ToastProvider duration={2500}>
+                  <Homepage />
+                </ToastProvider>
+              </ThemeProvider>
+            </div>
           </KitWalletProvider>
         </KitProvider>
       </QueryClientProvider>
